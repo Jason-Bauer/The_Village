9b6d67e76a55d059c7db121f446ce8b8be9255ec
DESKTOP-QGHGAFT\Jason <jcb5944@g.rit.edu>
2018-09-09 22:57:44 -0400

    Adding files

---
+++ Production S/Production Studio/Assets/Scriptss/CameraMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+   public Camera MainCam;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MainCam.transform.LookAt(new Vector3(0, 0, 0));
+    
+        MainCam.transform.Translate(Vector3.right * Time.deltaTime);
+    }
+}
---
+++ Production S/Production Studio/Assets/Scriptss/MenuControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuControl : MonoBehaviour {
+   public GameObject InfluenceMenu;
+	// Use this for initialization
+	void Start () {
+        InfluenceMenu.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OpenDropdown()
+    {
+        if (InfluenceMenu.activeSelf)
+        {
+            InfluenceMenu.SetActive(false);
+        }
+        else
+        {
+            InfluenceMenu.SetActive(true);
+        }
+    }
+}
