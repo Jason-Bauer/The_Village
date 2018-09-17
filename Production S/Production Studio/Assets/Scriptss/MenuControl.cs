844a9584c90fa49c89255ebd3f097a1cfd5c7103
DESKTOP-QGHGAFT\Jason <jcb5944@g.rit.edu>
2018-09-17 17:30:59 -0400

    progress 02

--- Production S/Production Studio/Assets/Scriptss/MenuControl.cs
+++ Production S/Production Studio/Assets/Scriptss/MenuControl.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuControl : MonoBehaviour {
    public GameObject InfluenceMenu;
+    public GameObject gather, war, tech;
+    public bool isgather, iswar, istech;
 	// Use this for initialization
 	void Start () {
         InfluenceMenu.SetActive(false);
+        isgather = false;
+        iswar = false;
+        istech =false;
+        war.GetComponent<Image>().color = Color.red;
+        gather.GetComponent<Image>().color = Color.red;
+        tech.GetComponent<Image>().color = Color.red;
     }
 	
 	// Update is called once per frame
@@ -24,5 +33,59 @@ public class MenuControl : MonoBehaviour {
         {
             InfluenceMenu.SetActive(true);
         }
+    }
+    public void flipgather()
+    {
+        if (isgather)
+        {
+            isgather = false;
+            gather.GetComponent<Image>().color = Color.red;
+        }
+        else if (!isgather)
+        {
+            isgather = true;
+            gather.GetComponent<Image>().color = Color.green;
+        }
+        else
+        {
+            Debug.Log("how?");
+        }
+
+    }
+    public void flipwar()
+    {
+        if (iswar)
+        {
+            iswar = false;
+            war.GetComponent<Image>().color = Color.red;
+        }
+        else if (!iswar)
+        {
+            iswar=true;
+            war.GetComponent<Image>().color = Color.green;
+        }
+        else
+        {
+            Debug.Log("hoWWWWW?");
+        }
+
+    }
+    public void fliptech()
+    {
+        if (istech)
+        {
+            istech = false;
+            tech.GetComponent<Image>().color = Color.red;
+        }
+        else if (!istech)
+        {
+            istech = true;
+            tech.GetComponent<Image>().color = Color.green;
+        }
+        else
+        {
+            Debug.Log("No really. how?");
+        }
+
     }
 }
