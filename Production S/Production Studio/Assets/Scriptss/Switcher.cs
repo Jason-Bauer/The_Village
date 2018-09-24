41a58415067d5ce1068398ca08406130f36b400e
DESKTOP-QGHGAFT\Jason <jcb5944@g.rit.edu>
2018-09-24 18:24:23 -0400

    progress 03

---
+++ Production S/Production Studio/Assets/Scriptss/Switcher.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Switcher : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void Switchscene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+	// Update is called once per frame
+	void Update () {
+		
+    
+	}
+}
